9e485c182704362065b93dd6172867a2268c4a99
Jakub Dipčár <jakub.dipcar@gmail.com>
2020-11-12 21:21:18 +0100

    som genius

--- Open-Lab-04.09/ArrayTools.cs
+++ Open-Lab-04.09/ArrayTools.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Principal;
 
 namespace Open_Lab_04._09
 {
@@ -6,7 +7,34 @@ namespace Open_Lab_04._09
     {
         public string[] RemoveDups(string[] strings)
         {
-            throw new NotImplementedException();
-        }
+            for (int i = 0; i < strings.Length; i++)
+            {
+                for (int ii = i + 1; ii < strings.Length; ii++)
+                {
+                    if (strings[i] == strings[ii])
+                    {
+                        strings[ii] = null;
+                    }
+                }
+            }
+            int a = 0;
+            int b;
+            for (int y = 0; y < strings.Length; y++)
+            {
+                if (strings[y] != null)
+                {
+                    string temp = strings[y];
+                    strings[y] = null;
+                    strings[a] = temp;
+                    a = a + 1;
+                }
+            }
+            string[] vysledok = new string[a];
+            for (int yy = 0; yy < a; yy++)
+            {
+                vysledok[yy] = strings[yy];
+            }
+            return vysledok;
+       }       
     }
 }
